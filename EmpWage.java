01daa35b45b1b0c8f70e771f775eb645c360c207
unknown <kumar.skm03@gmail.com>
2021-02-22 16:12:28 +0530

    added part time employee in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,8 @@
 
 public class EmpWage {
 
-	public static final int IS_EMPLOYEE_PRESENT = 1;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
 
 	public static void main(String args[]) {
@@ -9,11 +10,14 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wages Computation Program");
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck = (int) (Math.random() * 2);
-		if (empCheck == IS_EMPLOYEE_PRESENT)
+		int empCheck = (int) (Math.random() * 3);
+		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
+
 		empWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Employee daily wage: " + empWage);
 	}
