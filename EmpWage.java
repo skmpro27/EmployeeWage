f7079f3015e5efba21aebb72ad5a214c8d81deab
unknown <kumar.skm03@gmail.com>
2021-02-22 16:18:17 +0530

    added switch case in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -11,13 +11,19 @@ public class EmpWage {
 		int empHrs = 0;
 		int empWage = 0;
 		int empCheck = (int) (Math.random() * 3);
-		if (empCheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
+		switch (empCheck)
+		{
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
 
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+
+			default:
+				empHrs = 0;
+		}
 		empWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Employee daily wage: " + empWage);
 	}
