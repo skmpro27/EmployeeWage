3ae60d784e7427aa86c3173aef0f7838bf4d9431
unknown <kumar.skm03@gmail.com>
2021-02-22 16:06:05 +0530

    added daily wage of employee in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -2,14 +2,19 @@
 public class EmpWage {
 
 	public static final int IS_EMPLOYEE_PRESENT = 1;
+	public static final int WAGE_PER_HOUR = 20;
 
 	public static void main(String args[]) {
 
 		System.out.println("Welcome to Employee Wages Computation Program");
+		int empHrs = 0;
+		int empWage = 0;
 		int empCheck = (int) (Math.random() * 2);
 		if (empCheck == IS_EMPLOYEE_PRESENT)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs = 0;
+		empWage = empHrs * WAGE_PER_HOUR;
+		System.out.println("Employee daily wage: " + empWage);
 	}
 }
