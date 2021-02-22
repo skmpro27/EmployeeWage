e4fe33446f547a464d6f4a9e2f74df12c3f15c76
unknown <kumar.skm03@gmail.com>
2021-02-22 15:56:50 +0530

    added employee present or not in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,15 @@
 
 public class EmpWage {
-		public static void main(String args[]) {
+
+	public static final int IS_EMPLOYEE_PRESENT = 1;
+
+	public static void main(String args[]) {
 
 		System.out.println("Welcome to Employee Wages Computation Program");
+		int empCheck = (int) (Math.random() * 2);
+		if (empCheck == IS_EMPLOYEE_PRESENT)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
