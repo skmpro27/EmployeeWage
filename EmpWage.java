ce49ae107ade8dc475bda5721a1aac4c7fb9b5d0
unknown <kumar.skm03@gmail.com>
2021-02-24 22:38:36 +0530

    added multiple comnies in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -4,18 +4,23 @@ public class EmpWage {
         public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
         public final int WAGE_PER_HOUR;
+        public final int NUM_WORKING_DAYS;
+        public final int NUM_WORKING_HOURS;
+        public final String COMPANY;
 
-        public EmpWage(int perHr) {
+        public EmpWage(String str, int perHr, int days, int hrs) {
 
-                System.out.println("Welcome to Employee Wages Computation Program");
+		COMPANY = str;
                 WAGE_PER_HOUR = perHr;
+		NUM_WORKING_DAYS = days;
+		NUM_WORKING_HOURS = hrs;
         }
 
-        public int EmpWageCondition() {
+        public void empWageCondition() {
 
                 int empHrs = 0;
                 int workingDay = 0;
-                while (workingDay < 20 && empHrs < 100)
+                while (workingDay < NUM_WORKING_DAYS && empHrs < NUM_WORKING_HOURS)
                 {
                         workingDay++;
                         int empCheck = (int) (Math.random() * 3);
@@ -31,17 +36,23 @@ public class EmpWage {
 
                                 default:
                         }
-                        if (empHrs > 100)
-                                empHrs = 100;
+                        if (empHrs > NUM_WORKING_HOURS)
+                                empHrs = NUM_WORKING_HOURS;
                 }
+                System.out.println();
                 System.out.println("Number of Working Days: " + workingDay);
                 System.out.println("Number of Working Hours: " + empHrs);
-                return empHrs * WAGE_PER_HOUR;
+                System.out.println("Employee wages: " + (empHrs * WAGE_PER_HOUR));
         }
 
         public static void main(String args[]) {
 
-                EmpWage obj = new EmpWage(20);
-                System.out.println("Employee wages: " + obj.EmpWageCondition());
+		System.out.println("Welcome to Employee Wages Computation Program");
+
+                EmpWage dMart = new EmpWage( "DMart", 20, 20, 100);
+                EmpWage reliance = new EmpWage("Reliance", 30, 25, 120);
+
+		dMart.empWageCondition();
+		reliance.empWageCondition();
         }
 }
