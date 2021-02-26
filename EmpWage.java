d4ed7221890e18d736880531f0399a987d8e0aa4
unknown <kumar.skm03@gmail.com>
2021-02-26 22:10:57 +0530

    added array of company to handle multiple wage in  EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,28 +1,42 @@
 
+class CompanyStandard {
+        public final int wagePerHr;
+        public final int numWorkingDays;
+        public final int numWorkingHrs;
+        public  final String companyName;
+        public  int totalEmpWage;
+
+        public CompanyStandard(String companyName, int wagePerHr, int numWorkingDays, int numWorkingHrs) {
+
+                this.companyName = companyName;
+                this.wagePerHr = wagePerHr;
+                this.numWorkingDays = numWorkingDays;
+                this.numWorkingHrs = numWorkingHrs;
+        }
+
+        public void setTotalWage(int totalEmpWage) {
+                this.totalEmpWage = totalEmpWage;
+        }
+
+        @Override
+        public String toString() {
+                return "Employee wages of " + companyName + ": " + totalEmpWage;
+        }
+}
+
 public class EmpWage {
 
         private static final int IS_FULL_TIME = 1;
         private static final int IS_PART_TIME = 2;
 
-	private final int wagePerHr;
-        private final int numWorkingDays;
-        private final int numWorkingHrs;
-        private final String companyName;
-	private int totalEmpWage;
+        private static int numOfCompany = 0;
+        private static CompanyStandard company[] = new CompanyStandard[5];
 
-        public EmpWage(String companyName, int wagePerHr, int numWorkingDays, int numWorkingHrs) {
-
-		this.companyName = companyName;
-                this.wagePerHr = wagePerHr;
-		this.numWorkingDays = numWorkingDays;
-		this.numWorkingHrs = numWorkingHrs;
-        }
-
-        private void empWageCompute() {
+        private static void empWageCompute(int i) {
 
                 int empHrs = 0;
                 int workingDay = 0;
-                while (workingDay < numWorkingDays && empHrs < numWorkingHrs)
+                while (workingDay < company[i].numWorkingDays && empHrs < company[i].numWorkingHrs)
                 {
                         workingDay++;
                         int empCheck = (int) (Math.random() * 3);
@@ -38,29 +52,29 @@ public class EmpWage {
 
                                 default:
                         }
-                        if (empHrs > numWorkingHrs)
-                                empHrs = numWorkingHrs;
+                        if (empHrs > company[i].numWorkingHrs)
+                                empHrs = company[i].numWorkingHrs;
                 }
                 System.out.println("Working Days: " + workingDay + " and Working Hours: " + empHrs);
-		totalEmpWage = empHrs * wagePerHr;
+                int totalWage = empHrs * company[i].wagePerHr;
+	        company[i].setTotalWage(totalWage);
         }
-
-	@Override
-	public String toString() {
-		return "Employee wages of " + companyName + ": " + totalEmpWage;
-	}
 
         public static void main(String args[]) {
 
-		System.out.println("Welcome to Employee Wages Computation Program");
+                System.out.println("Welcome to Employee Wages Computation Program");
 
-                EmpWage dMart = new EmpWage( "DMart", 20, 20, 100);
-                EmpWage reliance = new EmpWage("Reliance", 30, 25, 120);
-
-		dMart.empWageCompute();
-		reliance.empWageCompute();
-
-		System.out.println(dMart);
-		System.out.println(reliance);
+                company[numOfCompany] = new CompanyStandard( "DMart", 20, 20, 100);
+		empWageCompute(numOfCompany);
+                System.out.println(company[numOfCompany]);
+		numOfCompany++;
+                company[numOfCompany] = new CompanyStandard("Reliance", 30, 25, 120);
+                empWageCompute(numOfCompany);
+                System.out.println(company[numOfCompany]);
         }
 }
+
+
+
+
+
