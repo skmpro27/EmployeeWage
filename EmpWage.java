15743a40768d016e8637390f34b3bacc83182e8f
unknown <kumar.skm03@gmail.com>
2021-02-26 23:13:13 +0530

    added interface and method to add Company use in EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,4 +1,11 @@
 
+interface IEmpWageCompute {
+	static void addCompany(String companyName, int wagePerHr, int numWorkingDays, int numWorkingHrs) {
+	}
+        static public void empWageCompute(int i) {
+	}
+}
+
 class CompanyStandard {
         public final int wagePerHr;
         public final int numWorkingDays;
@@ -24,7 +31,7 @@ class CompanyStandard {
         }
 }
 
-public class EmpWage {
+public class EmpWage implements IEmpWageCompute {
 
         private static final int IS_FULL_TIME = 1;
         private static final int IS_PART_TIME = 2;
@@ -32,7 +39,14 @@ public class EmpWage {
         private static int numOfCompany = 0;
         private static CompanyStandard company[] = new CompanyStandard[5];
 
-        private static void empWageCompute(int i) {
+	public static void addCompany(String companyName, int wagePerHr, int numWorkingDays, int numWorkingHrs) {
+                company[numOfCompany] = new CompanyStandard( companyName, wagePerHr, numWorkingDays, numWorkingHrs);
+		empWageCompute(numOfCompany);
+                System.out.println(company[numOfCompany]);
+		numOfCompany++;
+	}
+
+        public static void empWageCompute(int i) {
 
                 int empHrs = 0;
                 int workingDay = 0;
@@ -64,17 +78,7 @@ public class EmpWage {
 
                 System.out.println("Welcome to Employee Wages Computation Program");
 
-                company[numOfCompany] = new CompanyStandard( "DMart", 20, 20, 100);
-		empWageCompute(numOfCompany);
-                System.out.println(company[numOfCompany]);
-		numOfCompany++;
-                company[numOfCompany] = new CompanyStandard("Reliance", 30, 25, 120);
-                empWageCompute(numOfCompany);
-                System.out.println(company[numOfCompany]);
+                addCompany( "DMart", 20, 20, 100);
+                addCompany("Reliance", 30, 25, 120);
         }
 }
-
-
-
-
-
